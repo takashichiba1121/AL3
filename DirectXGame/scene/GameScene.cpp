562aeb77afcb8e1ec77614e93b5a919ff66c67fb
takashithiba <k021g1120@g.neec.ac.jp>
2022-04-21 13:37:48 +0900

    コミット

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -1,6 +1,7 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include<random>
 
 using namespace DirectX;
 
@@ -19,46 +20,128 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 	//ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("mario.jpg");
+
 	//スプライトの生成
 	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
+
 	//3Dモデルの生成
 	model_ = Model::Create();
-	//ワールドトランスフォームの初期化
-	worldTransform_.Initialize();
+
+	//乱数シード生成器
+	std::random_device seed_gen;
+
+	//メルセンヌ・ツイスター
+	std::mt19937_64 engine(seed_gen());
+
+	//乱数範囲（回転角用）
+	std::uniform_real_distribution<float>rotDist(0.0f, XM_2PI);
+
+	// 乱数範囲（座標用）
+	std::uniform_real_distribution<float>posDist(-10.0f, 10.0f);
+	for (size_t i = 0; i < _countof(worldTransform_); i++)
+	{
+		//X,Y,Z方向のスケーリングを設定
+		worldTransform_[i].scale_ = {5.0f,5.0f,5.0f};
+
+		//X,Y,Z,　軸回りの回転角を設定
+		worldTransform_[i].rotation_ = { rotDist(engine),rotDist(engine),rotDist(engine) };
+
+		//X,Y,Z 軸回りの平行移動を設定
+		worldTransform_[i].translation_ = { posDist(engine),posDist(engine),posDist(engine) };
+
+		//ワールドトランスフォームの初期化
+		worldTransform_[i].Initialize();
+	}
+
+	//カメラ視点座標を設定
+	viewProjection_.eye = { 0,0,-10 };
+
 	//ビュープロダクションの初期化
 	viewProjection_.Initialize();
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
-	//音声再生
-	audio_->PlayWave(soundDataHandle_);
-	//音声再生
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	//カメラ注視点座標を設定
+	viewProjection_.target = { 10,0,0 };
+
+	//カメラ上方向ベクトルを設定（右上４５度指定）
+	viewProjection_.up = { cosf(XM_PI / 4.0f),sinf(XM_PI / 4.0f),0.0f };
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
 }
 
 void GameScene::Update() {
-	XMFLOAT2 position = sprite_->GetPosition();
-	//座標を{2,0}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
-	//スペースキーを押して瞬間
-	if (input_->TriggerKey(DIK_SPACE)) {
-		//音声停止
-		audio_->StopWave(voiceHandle_);
+	////書式指定付き表示
+	//debugText_->SetPos(50, 70);
+	//debugText_->Printf("translation_:(%f,%f,%f)",worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
+	//debugText_->SetPos(50, 87);
+	//debugText_->Printf("rotation_:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y, worldTransform_.rotation_.z);
+	//debugText_->SetPos(50, 104);
+	//debugText_->Printf("scale_:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y, worldTransform_.scale_.z);
+
+	//視点移動処理
+	//視点移動ベクトル
+	XMFLOAT3 move = { 0,0,0 };
+
+	//視点の移動速さ
+	const float kEyeSpeed = 0.2f;
+
+	//押した方向ベクトルを変更
+	if (input_->PushKey(DIK_W)) {
+		move = { 0,0,kEyeSpeed };
 	}
-	//デバッグテキストの表示
-	debugText_->Print("kaizokuou ni oreha naru", 50, 50, 1.0f);
-	//書式指定付き表示
+	if (input_->PushKey(DIK_S)) {
+		move = { 0,0,-kEyeSpeed };
+	}
+	//視点移動（ベクトルの加算）
+	viewProjection_.eye.x += move.x;
+	viewProjection_.eye.y += move.y;
+	viewProjection_.eye.z += move.z;
+
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+	//注視点移動処理
+	XMFLOAT3 kmove = { 0,0,0 };
+
+	//注視点の移動速さ
+	const float kTargetSpeed = 0.2f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_LEFT)) {
+		kmove = { -kTargetSpeed,0,0 };
+	}
+	else if (input_->PushKey(DIK_RIGHT)) {
+		kmove = { kTargetSpeed,0,0 };
+	}
+
+	//注視点移動（ベクトルの加算）
+	viewProjection_.target.x += kmove.x;
+	viewProjection_.target.y += kmove.y;
+	viewProjection_.target.z += kmove.z;
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+	//上方向の回転速さ[ラジアン/frame]
+	const float kUpRotSpeed = 0.05f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_SPACE)) {
+		viewAngle += kUpRotSpeed;
+		//２πを超えたら０に戻す
+		viewAngle = fmodf(viewAngle,XM_2PI);
+	}
+	//上方向ベクトルを計算（半径１の円周上の座標）
+	viewProjection_.up = { cosf(viewAngle),sinf(viewAngle),0.0f};
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+	//デバッグ用表示
+	debugText_->SetPos(50, 50);
+	debugText_->Printf("eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
 	debugText_->SetPos(50, 70);
-	debugText_->Printf("year:%d", 2001);
-	//変数の値をインクリメント
-	value_++;
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") +
-		std::to_string(value_);
-	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 50, 1.0f);
+	debugText_->Printf("target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y, viewProjection_.target.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf("target:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
 }
 
 void GameScene::Draw() {
@@ -87,7 +170,11 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+
+	for (size_t i = 0; i < _countof(worldTransform_); i++)
+	{
+		model_->Draw(worldTransform_[i], viewProjection_, textureHandle_);
+	}
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -100,7 +187,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -54,7 +54,7 @@ class GameScene {
 	//3Dモデル
 	Model* model_ = nullptr;
 	//ワールドトランスフォーム
-	WorldTransform worldTransform_;
+	WorldTransform worldTransform_[100];
 	//ビュープロダクション
 	ViewProjection viewProjection_;
 	//サウンドデータハンドル
@@ -63,6 +63,8 @@ class GameScene {
 	uint32_t voiceHandle_ = 0;
 	//値を表示したい変数
 	int32_t value_ = 0;
+	//カメラ上方向の角度
+	float viewAngle = 0.0f;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
