27b6be8f624f9215c2cf98f05d31bfc82e733863
takashithiba <k021g1120@g.neec.ac.jp>
2022-06-13 11:36:06 +0900

    終了

--- DirectXGame/PlayerBullet.cpp
+++ DirectXGame/PlayerBullet.cpp
@@ -9,7 +9,7 @@ void PlayerBullet::Initialize(Model* model, const Vector3& position)
 
 	model_ = model;
 	//テクスチャ読み込み
-	textureHandle_ = TextureManager::Load("mario.jpg");
+	textureHandle_ = TextureManager::Load("black.png");
 
 	worldTransform_.Initialize();
 
